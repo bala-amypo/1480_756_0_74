2c3fcdf3de4fdbdd201f2da647e3b9fee2d6c8f5
bala-amypo <bala@amypo.in>
2025-12-17 10:44:18 +0000

    User commit

--- src/main/java/com/example/demo/config/swaggerconfig.java
+++ src/main/java/com/example/demo/config/swaggerconfig.java
@@ -14,7 +14,7 @@ public class swaggerconfig {
         return new OpenAPI()
                 // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9223.408procr.amypo.ai/")
+                        new Server().url("https://9181.408procr.amypo.ai")
                 ));
         }
 }

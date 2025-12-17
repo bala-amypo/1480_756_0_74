c45c8d97592ac9f22e10b685a2adbad377b5cf7c
bala-amypo <bala@amypo.in>
2025-12-17 08:46:31 +0000

    User commit

--- src/main/java/com/example/demo/service/LocationServiceImp.java
+++ src/main/java/com/example/demo/service/LocationServiceImp.java
@@ -1,15 +1,26 @@
 package com.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.demo.entity.LocationEntity;
+import com.example.demo.repository.LocationRepository;
 
 public class LocationServiceImp implements LocationService {
     @Autowired
-    LocationRepository Locationrepo;
+    LocationRepository locationrepo;
     @Override
     public LocationEntity createlocation(LocationEntity le){
-        return Locationrepo.save(le);
+        return locationrepo.save(le);
     }
-
+    public List<LocationEntity> getall(){
+        return locationrepo.findAll();
+    }
+    @Override
+    public List<LocationEntity> getalllocation() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getalllocation'");
+    }
+ 
 }
